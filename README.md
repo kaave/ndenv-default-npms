e3bf326d453ae0c2696bb8b672878b51aa74a80d
kaave <kyousuke.abe@aliinc.co.jp>
2016-06-21 20:24:50 +0900

    Add operation method

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-# UNDER CONSTRUCTION
-
-<!--
 # ndenv-default-npms
 
-it is copy from [rbenv-default-gems](https://github.com/rbenv/rbenv-default-gems).
--->
+This [ndenv](https://github.com/riywo/ndenv) plugin is simular to [rbenv-default-gems](https://github.com/rbenv/rbenv-default-gems): It hooks into the `ndenv install` command to automatically install npms to global on every time you install a new version of Node.js or io.js.
 
+## How to install
+
+```
+git clone https://github.com/kaave/ndenv-default-npms.git $(ndenv root)/plugins/ndenv-default-npms
+```
+
+## Add `default-npms` file
+
+Create text file to `$(ndenv root)/default-npms` and add your need packages.
+
+```text
+#----------------
+# Example setting
+#----------------
+
+# Lines beginning with a # are comment line.
+
+npm
+# => npm i -g npm
+
+coffee-script@1.9.0
+# => npm i -g coffee-script@1.9.0
+
+typescript@next
+# => npm i -g typescript@next
+```
--- etc/ndenv.d/install/default-npms.bash
+++ etc/ndenv.d/install/default-npms.bash
@@ -1,36 +1,24 @@
 after_install install_default_npms
 
 install_default_npms() {
+    # check latest node or iojs install status
     [ "$STATUS" = "0" ] || return 0;
-    [ ! -f "${NDENV_ROOT}/default-npms" ] || return 0;
+    # check setting file
+    [ -f "${NDENV_ROOT}/default-npms" ] || return 0;
 
+    echo "ndenv-default-npms install start";
+    while IFS= read -ra package_name; do
+        # check empty line
+        [ "${#package_name[@]}" -gt 0 ] || continue
+        # check comment line
+        [ "${package_name[0]:0:1}" != "#" ] || continue
 
-    while IFS=" " read -r -a line; do
-      # Skip empty lines
-      [ "${#line[@]}" -gt 0 ] || continue
+        echo "install start: $package_name";
 
-      # Skip comment lines that begin with `#`.
-      [ "${line[0]:0:1}" != "#" ] || continue
-
-
-      # gem_name="${line[0]}"
-      # gem_version="${line[1]}"
-      #
-      # if [ "$gem_version" == "--pre" ]; then
-      #   args=( --pre )
-      # elif [ -n "$gem_version" ]; then
-      #   args=( --version "$gem_version" )
-      # else
-      #   args=()
-      # fi
-      #
-      # # Invoke `gem install` in the just-installed Ruby. Point its
-      # # stdin to /dev/null or else it'll read from our default-gems
-      # # file.
-      # RBENV_VERSION="$VERSION_NAME" rbenv-exec gem install "$gem_name" "${args[@]}" < /dev/null || {
-      #   echo "rbenv: error installing gem \`$gem_name'"
-      # } >&2
+        NDENV_VERSION="$VERSION_NAME" ndenv exec npm i -g "$package_name" < /dev/null || {
+          echo "ndenv: error installing npm \`$package_name'"
+        } >&2
 
     done < "${NDENV_ROOT}/default-npms"
+    echo "ndenv-default-npms install finish";
 }
-
